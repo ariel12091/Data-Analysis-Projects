b175be5486155666f1d35c4ee662ff1a196d528a
unknown <ariel.t@maisha-group.com>
2023-03-10 12:02:15 +0200

    NEW script

---
+++ stat_padding.R
@@ -0,0 +1,49 @@
+library(rvest)
+library(dplyr)
+library(tidyverse)
+library("hoopR")
+
+future::plan("multisession")
+pbp1 <- hoopR::load_nba_pbp(seasons = c(2002:2023))
+
+
+
+list_gamed_ids <- pbp %>%
+  distinct(game_id) %>%
+  pull()
+
+all_box_score <- load_nba_player_box(seasons = c(2002:2023))
+
+  trip_dub_pbp <- all_box_score %>%
+  filter(as.numeric(pts) >= 10 & as.numeric(reb) == 10 & as.numeric(ast) >=10) %>%
+  inner_join(pbp1, by = c("athlete_id" = "participants_0_athlete_id", "game_id")) %>%
+  filter(type_id %in% c('155', '156')) %>%
+  group_by(athlete_id, game_id) %>%
+  mutate(num_reb = row_number()) %>%
+  relocate(c(num_reb, end_game_seconds_remaining, qtr, time, clock_minutes, clock_seconds, game_id), .after = athlete_display_name) %>%
+  ungroup() %>%
+  filter(num_reb == 10) %>%
+  mutate(under_minute = end_game_seconds_remaining < 50) %>%
+  #  filter(under_minute == TRUE & athlete_id == 3907387) %>%
+  #  relocate(home_team_name, .after = home_score) %>%
+  #  view()
+  group_by(under_minute, athlete_display_name, athlete_id) %>%
+  summarise(n = n()) %>%
+  arrange(desc(n)) %>%
+  ungroup() %>%
+  group_by(athlete_display_name, athlete_id) %>%
+  mutate(freq = n / sum(n)) %>%
+  ungroup() %>%
+  filter(under_minute == TRUE) %>%
+  inner_join(all_box_score %>%
+               filter(as.numeric(pts) >= 10 & as.numeric(reb) >=10 & as.numeric(ast) >= 10) %>%
+               group_by(athlete_id, athlete_display_name) %>%
+               summarise(n_total_trips = n_distinct(game_id)) %>%
+               ungroup() %>%
+               arrange(desc(n_total_trips)), by = c("athlete_id", "athlete_display_name")) %>%
+  mutate(freq_under_minute = n / n_total_trips) %>%
+  view()
+
+group_by(qtr, clock_minutes, clock_seconds, athlete_display_name) %>%
+  summarise(n = n()) %>%
+  view()
